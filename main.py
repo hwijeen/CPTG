fix a bug in loss computation

main.py
@@ -18,7 +18,8 @@ device = torch.device('cuda:0')
 multi_gpu = True
 
 
-setproctitle("(hwijeen) CPTG in progress")
+exp_name = "(hwijeen)CPTG debugged generator loss"
+setproctitle(exp_name)
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     datefmt = '%m/%d/%Y %H:%M:%S', level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -26,12 +27,13 @@ logger = logging.getLogger(__name__)
 
 if  __name__ == "__main__":
 
-    data = build_data(DATA_DIR, batch_size=32, device=device)
+    data = build_data(DATA_DIR, batch_size=64, device=device)
     cptg = make_model(len(data.vocab), len(data.attr), device=device)
-    trainer = Trainer(cptg, data)
+    trainer = Trainer(cptg, data, lambda_=1.0)
     print(cptg)
 
     trainer.train(epoch=5)
+    print(exp_name)
 
 
 

model.py
@@ -4,8 +4,8 @@ import torch
 import torch.nn as nn
 from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
 
-from utils import make_one_hot, truncate, append
-from dataloading import SOS_IDX, EOS_IDX
+from utils import make_one_hot, truncate, append, sequence_mask, sort_by_length
+from dataloading import SOS_IDX, EOS_IDX, PAD_IDX
 
 
 # TODO: generation max length from shen et al.
@@ -26,17 +26,20 @@ class CPTG(nn.Module):
         l_: (B, )
         """
         # TODO: come up with a more readable code for generation
-        try:
-            hx, hy, gen_output = self.generator(x, l, l_, is_gen)
-        except: # when generating
+        #try:
+        #    hx, hy, gen_output = self.generator(x, l, l_, is_gen)
+        #except ValueError: # when generating
+        #    y = self.generator(x, l, l_, is_gen)
+        #    return y
+        if is_gen:
             y = self.generator(x, l, l_, is_gen)
             return y
+        else:
+            hx, hy, gen_output = self.generator(x, l, l_, is_gen)
         dis_output = self.discriminator(hx, hy, l, l_)
         return gen_output, dis_output # tuple (B, MAXLEN, 700), (B,) and
                                       # tuple of hx_l, hy_l_, hx_l_
                                       # hx_l: (B,)
-
-
 class Generator(nn.Module):
     def __init__(self, encoder, decoder, gamma):
         super().__init__()
@@ -46,18 +49,18 @@ class Generator(nn.Module):
 
     def _fuse(self, z_x, z_y):
         g = torch.empty_like(z_x).bernoulli_(self.gamma) # (B, 500)
-        z_xy = (g * z_x) - ((g != 1).float() * z_y)
+        z_xy = (g * z_x) + ((1 - g) * z_y)
         return z_xy # (B, 500)
 
     def forward(self, x, l, l_, is_gen):
         """
         x: tuple of (B, L+1) and (B,)
-        l: (B, )
-        l_: (B, )
+        l: (B,)
+        l_: (B,)
         """
         z_x = self.encoder(x)
         hy, y = self.decoder(z_x, l_)
-        if  is_gen:
+        if is_gen:
             return y
         z_y = self.encoder(y)
         z_xy = self._fuse(z_x, z_y)
@@ -96,8 +99,22 @@ class Decoder(nn.Module):
         # output (B, 1, vocab)
         prob = output.squeeze(1).softmax(dim=-1)
         sampled = torch.multinomial(prob, num_samples=1)
-        len_ = (sampled != EOS_IDX).squeeze(1).long()
-        return sampled.detach(), len_ # (B, 1), (B,)
+        #len_ = (sampled != EOS_IDX).squeeze(1).long()
+        return sampled.detach()#, len_ # (B, 1), (B,)
+
+    def _tighten(self, hy, y):
+        """
+        pad tokens after EOS and mask hiddens after EOS
+        hy: (B, MAXLEN+1, 700
+        y: (B, MAXLEN+1)
+        """
+        # DEBUG: inconsistency in results with argmax
+        lengths = (y == EOS_IDX).argmax(dim=1) + 1 # (B,) +1 for 0 index
+        mask = sequence_mask(lengths)
+        y.masked_fill_((mask!=1), PAD_IDX) # this does not backprop
+        hy = hy * (mask.unsqueeze(-1)).float()
+        hy, y, lengths = sort_by_length(hy, y, lengths)
+        return hy, y, lengths
 
     def forward(self, z, l, x=None):
         """
@@ -121,34 +138,28 @@ class Decoder(nn.Module):
             output = self.out(hx)
             return (hx, lengths), (output, lengths) # (B, L+1, 700), (B,)
                                                     # (B, L+1, vocab), (B,)
-
         else: # sample y
             y = []
             hy = []
-            input_ = z.new_full((B, 1), SOS_IDX).long()
-            lengths = input_.new_zeros(B)
-            # FIXME: when generating y, how to handle EOS?
+            input_ = l.new_full((B, 1), SOS_IDX)
             for t in range(MAXLEN):
                 input_ = self.emb(input_) # (B, 1, 300)
                 # output (B, 1, 700), hidden (1, B, 700)
                 output, hidden = self.gru(input_, hidden)
-                input_, len_ = self._hard_sampling(self.out(output))
+                input_ = self._hard_sampling(self.out(output))
                 hy.append(output)
                 y.append(input_)
-                # TODO: calculate exact length with EOS consiered and sort
-                #lengths += len_
-                lengths += lengths.new_ones((1,)) # broadcasting
+            input_ = l.new_full((B,1), EOS_IDX) # feed <eos> as last input,
+            output, _ = self.gru(self.emb(input_), hidden)
+            hy.append(output)
+            y.append(input_) # append <eos> as last token
+
             hy = torch.cat(hy, dim=1)
             y = torch.cat(y, dim=1)
-            # y, lengths = self._tighten(y, lengths)
-            return (hy, lengths), (y, lengths) # (B, MAXLEN, 700), (B, MAXLEN), (B, )
-
-        # TODO: tighten sampled batch
-        def _tighten(self, y, lengths):
-            """
-            truncate tokens after EOS
-            """
-            pass
+            #hy, y, lengths = self._tighten(hy, y)
+            # FIXME: exact length with tighten
+            lengths = y.new_full((B,), MAXLEN+1)
+            return (hy, lengths), (y, lengths) # (B, MAXLEN+1, 700), (B, MAXLEN+1), (B, )
 
 
 class Discriminator(nn.Module):
@@ -162,7 +173,7 @@ class Discriminator(nn.Module):
 
     def _phi(self, h):
         """
-        h: tuple of (B, L, 700) and (B,)
+        h: tuple of (B, L+1, 700) and (B,)
         """
         h, lengths = h
         B, total_length, _ = h.size()
@@ -173,7 +184,6 @@ class Discriminator(nn.Module):
 
     def _discriminator(self, h, l):
         last_hidden = self._phi(h)
-        # FIXME: is l 'binary vector'?
         l_onehot = make_one_hot(l, self.attr)
         term1 = torch.sum(l_onehot * self.W(last_hidden), dim=1)
         term2 = torch.sum(self.v * last_hidden, dim=1)
@@ -186,8 +196,8 @@ class Discriminator(nn.Module):
         l: (B, )
         """
         hx_l = self._discriminator(hx, l)
-        hy_l_ = self._discriminator(hy, l_)
-        hx_l_ = self._discriminator(hx, l_) # FIXME: clone needed?
+        hy_l_ = self._discriminator(hy, l_) # FIXME: detach needed?!?!
+        hx_l_ = self._discriminator(hx, l_)
         return hx_l, hy_l_, hx_l_
 
 

trainer.py
@@ -30,26 +30,32 @@ class Trainer(object):
         real_loss = self.lambda_ * 2 * self.d_criterion(hx_l, real_label)
         fake_loss = self.lambda_ * (self.d_criterion(hy_l_, fake_label) +\
             self.d_criterion(hx_l_, fake_label))
-
         adv_loss = real_loss + fake_loss
 
         adv_loss.backward(retain_graph=True)
+        self.d_optimizer.zero_grad() # clear grad acculated from gen real loss
         self.d_optimizer.step()
         self.d_optimizer.zero_grad()
         return adv_loss
 
-    def _generator_step(self, gen_logit, x):
-        logit, _ = gen_logit # (B, L+1, vocab), (B,)
+    # TODO: generator adv loss
+    def _generator_step(self, gen_logit, dis_logit, x):
+        gen_logit, _ = gen_logit # (B, L+1, vocab), (B,)
+        B, L, _ = gen_logit.size()
+        hx_l, _, _ = dis_logit
         target, lengths = x # (B, L+1)
         target = torch.cat([t.view(-1) for t in target], dim=0)
         num_token = torch.sum(lengths).float()
+        real_label = hx_l.new_ones(B,)
 
-        B, L, _ = logit.size()
-        recon_loss = torch.sum(self.g_criterion(logit.view(B*L, -1),
+        recon_loss = torch.sum(self.g_criterion(gen_logit.view(B*L, -1),
                                           target.view(-1))) / num_token
+        real_loss = self.lambda_ * 2 * self.d_criterion(hx_l, real_label)
+        gen_loss = recon_loss + real_loss
 
-        recon_loss.backward()
-        self.g_optimizer.step()
+        # FIXME: detach hx, hy or not? detach can be performed in model.Descriminator
+        gen_loss.backward()     # some of the grads are from discriminator step
+        self.g_optimizer.step() # grad that backproped to g thru hx, hy
         self.g_optimizer.zero_grad()
         return recon_loss
 
@@ -60,7 +66,8 @@ class Trainer(object):
                 (x, lengths), l, l_ = prepare_batch(batch)
                 gen_logit, dis_logit = self.model((x, lengths), l, l_)
                 adv_loss = self._discriminator_step(dis_logit)
-                recon_loss = self._generator_step(gen_logit, (x, lengths))
+                recon_loss = self._generator_step(gen_logit, dis_logit,
+                                                  (x, lengths))
 
                 if step % 100 == 0:
                     logger.info('loss at epoch {}, step {}: {:.2f}'.format(
@@ -71,15 +78,13 @@ class Trainer(object):
 
     # TODO: early stopping
     def evaluate(self):
-        self.data.valid_iter.shuffle = True 
+        self.data.valid_iter.shuffle = True
         import random
         a = random.randint(0, len(self.data.valid_iter)) # temp test
         for i, batch in enumerate(self.data.valid_iter):
             if i != a: continue
-            # FIXME: feed l when inferring?
             (x, lengths), l, l_ = prepare_batch(batch)
             generated = self.model((x, lengths), l, l_, is_gen=True)
-            #decoded = self.decode(gen_logit)
             print('=' * 50)
             print('original \t\t -> \t\t changed')
             for idx in random.sample(range(lengths.size(0)), 5):
@@ -87,13 +92,8 @@ class Trainer(object):
                 chg = reverse(generated[0], self.data.vocab)[idx]
                 print(' '.join(ori))
                 print('\t\t->', ' '.join(chg))
-            #print('\n'.join([' '.join(ex) for ex in
-            #                 random.sample(reverse(x, self.data.vocab), 3)]))
-            #print('\n'.join([' '.join(ex) for ex in
-            #                random.sample(reverse(generated[0], self.data.vocab), 3)]))
             print('=' * 50)
             return
-
 
     #def decode(self, gen_logit):
         #pass

utils.py
@@ -4,20 +4,23 @@ from dataloading import EOS_IDX, SOS_IDX
 
 
 def make_one_hot(l, attr):
+    # make one-hot vector representation of a label
     B = l.size(0)
     one_hot = l.new_zeros(B, attr).float()
     one_hot[range(B), l] = 1
     return one_hot
 
+
 def prepare_batch(batch):
-    # attach the opposite label
+    # attach the opposite label to a batch
     x, lengths = batch.sent
     l = batch.label
-    B = x.size(0)
     l_ = (l != 1).long()
     return (x, lengths), l, l_
 
+
 def truncate(x, token=None):
+    # delete a special token in a batch
     assert token in ['sos', 'eos', 'both'], 'can only truncate sos or eos'
     x, lengths = x # (B, L)
     lengths -= 1
@@ -26,11 +29,13 @@ def truncate(x, token=None):
     else: x = x[:, 1:-1]
     return (x, lengths)
 
+
 def append(x, token=None):
+    # add a special token to a batch
     assert token in ['sos', 'eos'], 'can only append sos or eos'
     x, lengths = x # (B, L)
-    B = x.size(0)
     lengths += 1
+    B = x.size(0)
     if token == 'eos':
         eos = x.new_full((B,1), EOS_IDX)
         x = torch.cat([x, eos], dim=1)
@@ -39,7 +44,28 @@ def append(x, token=None):
         x = torch.cat([sos, x], dim=1)
     return (x, lengths)
 
+
+def sequence_mask(lengths):
+    # make a mask matrix corresponding to a given length
+    # from https://github.com/tensorflow/tensorflow/blob/r1.12/tensorflow/python/ops/array_ops.py
+    row_vector = torch.arange(0, max(lengths), device=lengths.device) # (L,)
+    matrix = lengths.unsqueeze(-1) # (B, 1)
+    result = row_vector < matrix # 1 for real tokens
+    return result # (B, L)
+
+
+def sort_by_length(hy, y, lengths):
+    # sort hy, y in decreasing order(for compatibility with packed_sequence)
+    l = [(hidden, tok, l) for hidden, tok, l in zip(hy, y, lengths)]
+    sorted_l = sorted(l, key=lambda x: x[2], reverse=True)
+    hy = torch.stack([i[0] for i in sorted_l], dim=0)
+    h = torch.stack([i[1] for i in sorted_l], dim=0)
+    lengths = torch.stack([i[2] for i in sorted_l], dim=0)
+    return hy, h, lengths
+
+
 def reverse(batch, vocab):
+    # turn a batch of idx to tokens
     batch = batch.tolist()
 
     def trim(s, t):
@@ -50,7 +76,6 @@ def reverse(batch, vocab):
             sentence.append(w)
         return sentence
     batch = [trim(ex, EOS_IDX) for ex in batch]
-
     batch = [[vocab.itos[i] for i in ex] for ex in batch]
     return batch
 